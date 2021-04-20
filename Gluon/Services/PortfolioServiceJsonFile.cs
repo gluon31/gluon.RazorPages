2a3a3ba84a1b7e009431b1d04d7c945dde138ea9
gluon31 <khirai2@gmail.com>
2021-04-20 22:31:41 +0900

    Add Json

---
+++ Gluon/Services/PortfolioServiceJsonFile.cs
@@ -0,0 +1,26 @@
+﻿using Gluon.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Gluon.Services
+{
+    public class PortfolioServiceJsonFile
+    {
+        public IEnumerable<Portfolio> GetPortfolios()
+        {
+            var jsonFileName = @"C:\Users\khira\source\repos\gluon.RazorPages\gluon.RazorPages\Gluon\wwwroot\Portfolios\portfolios.json";
+
+            using (var jsonFileReader = File.OpenText(jsonFileName))
+            {
+                var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+                var portfolios = JsonSerializer.Deserialize<Portfolio[]>(jsonFileReader.ReadToEnd(), options);
+                return portfolios;
+            }
+            
+        }
+    }
+}
